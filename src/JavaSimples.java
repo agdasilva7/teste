237cc6bb7c4355ffa813c20875ecb860ece6bd24
agdasilva7 <agdasilva493@gmail.com>
2024-06-24 08:03:08 -0300

    primeiro commit

---
+++ src/JavaSimples.java
@@ -0,0 +1,5 @@
+public class JavaSimples {
+    public static void main(String[] args) throws Exception {
+        System.out.println("olá, essa é uma aplicação simples de java para mostrar o uso do git!");
+    }
+}
